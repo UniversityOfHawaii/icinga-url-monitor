7e5d7c33d04a3ae3d827b8b35103797d7c1eb052
Wes Price <wprice@hawaii.edu>
2015-02-24 15:09:19 -1000

    added documentation

--- url_reachability_tester.pl
+++ url_reachability_tester.pl
@@ -6,9 +6,6 @@
 ## Author:        wes price <wprice@hawaii.edu>
 ## Created at:    Thu Feb  19 15:30 2015
 ######################################################################
-
-
-
 use strict;
 use warnings;
 
@@ -21,7 +18,7 @@ use File::Basename;
 use LWP::UserAgent;
 use Monitoring::Plugin;
 
-my $DEFAULT_TIMEOUT = 60;
+my $DEFAULT_TIMEOUT = 15;
 my $VERSION = '1.0';
 
 # get the base name of this script for use in the examples
@@ -30,9 +27,13 @@ my $PROGNAME = basename($0);
 
 # instantiate Monitoring::Plugin
 my $p = Monitoring::Plugin->new(
-    usage => "Usage: %s [ -v|--verbose ]  [-t <timeout>] -u <url> | --url <url> [-u <url> | --url <url> ...]",
+    usage => "Usage: %s -n <name> -u <url> [-t <timeout>] [-v]",
     version => $VERSION,
-    shortname => $PROGNAME
+    shortname => $PROGNAME,
+    license =>
+"This icinga plugin was developed for monitoring the availability of web
+applications developed by UH Manoa ITS, and may be freely used,
+redistributed, and/or modified."
 );
 
 
@@ -43,7 +44,15 @@ $p->add_arg(
     spec => 'url|u=s',
     help =>
 qq{-u, --url=STRING
-   Specify the full url being tested},
+   The url being tested},
+   required => 1
+);
+
+$p->add_arg(
+    spec => 'name|n=s',
+    help =>
+qq{-n, --name=STRING
+   The name of the application being tested},
    required => 1
 );
 
@@ -57,16 +66,17 @@ if (defined $timeout) {
         $p->plugin_die( " timeout must be >= 0" );
     }
     else {
-        print " set timeout to $timeout\n " if $p->opts->verbose;
+        print "set timeout to $timeout\n" if $p->opts->verbose;
     }
 } else {
     $timeout = $DEFAULT_TIMEOUT;
-    print " using default timeout of $timeout\n " if $p->opts->verbose;
+    print "using default timeout of $timeout\n" if $p->opts->verbose;
 }
 
+my $name = $p->opts->name;
 my $url = $p->opts->url;
 if (defined(is_web_uri( $url ))) {
-    print " validated URL \'$url\'\n " if $p->opts->verbose;
+    print "validated URL \'$url\'\n" if $p->opts->verbose;
 }
 else {
     $p->plugin_die( " invalid URL \'$url\'" );
@@ -75,22 +85,25 @@ else {
 
 # perform the actual reachability test
 my $ua = LWP::UserAgent->new;
-$ua->agent("$PROGNAME/$VERSION ");
+$ua->agent("$PROGNAME/$VERSION");
 $ua->timeout($timeout);
 
 my $req = HTTP::Request->new(GET => $url);
+print "sending request to $name ($url)\n" if ($p->opts->verbose > 1);
 my $res = $ua->request($req);
-print Dumper($res) if ($p->opts->verbose > 1);
+print "received response from $name ($url)\n" if ($p->opts->verbose > 1);
+
+print Dumper($res) if ($p->opts->verbose > 2);
 
 my ($resultCode, $message);
 
 if ($res->is_success) {
     $resultCode = 0;
-    $message = "contacted URL \'$url\'";
+    $message = "contacted \'$name\' ($url)";
 }
 else {
     $resultCode = 2;
-    $message = $res->status_line;
+    $message = "error contacting \'$name\' ($url): " . $res->status_line;
 }
 
 
@@ -100,5 +113,56 @@ else {
 # 2 == critical
 $p->plugin_exit(
     return_code => $resultCode,
-    message => " $message"
+    message => $message
 );
+
+__END__
+
+=head1 url_reachability_tester
+
+url_reachability_tester -- verifies that the given website URL can be reached 
+
+=head1 SYNOPSIS
+
+Usage: url_reachability_tester -n <name> -u <url> [-t <timeout>] [-v]
+
+=head1 DESCRIPTION
+
+Icinga-compatible plugin script which Uses LWP::UserAgent to connect to the
+given URL, returning OK status if the connection attempt succeeds (with an
+HTTP result code of 200), CRITICAL status for any other result (timeout,
+invalid URL, or HTTP status code != 200)
+
+The following command-line parameters are supported:
+
+=over 4
+
+=item  -?, --usage
+Print usage information
+
+=item -h, --help
+Print detailed help screen
+
+=item -V, --version
+Print version information
+
+=item -n, --name=STRING (required)
+Specify the name of the application being tested
+
+=item -u, --url=STRING (required)
+Specify the url being tested
+
+=item -t, --timeout=INTEGER
+Seconds before plugin times out (default: 15)
+
+=item -v, --verbose
+Print debugging-related details during execution (can repeat up to 3 times
+to increase verbosity)
+
+=back
+
+=head1 AUTHOR
+
+Developed and maintained by Wes Price (wprice@hawaii.edu), UH ITS/MIS
+
+=cut
